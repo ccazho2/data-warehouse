ddbd9c664f1dffb1b8069e6b9f4eaf7db4554475
ccazho2 <ccazho2@gmail.com>
2026-07-20 16:17:52 -0500

    Update load_silver_procedure.sql
    transformations accounting for carriage return characters within value fields

--- scripts/silver/load_silver_procedure.sql
+++ scripts/silver/load_silver_procedure.sql
@@ -161,93 +161,93 @@ BEGIN
 
         -- Loading erp_cust_az12
         SET @start_time = GETDATE();
-    		PRINT '>> Truncating Table: silver.erp_cust_az12';
-    		TRUNCATE TABLE silver.erp_cust_az12;
-    		PRINT '>> Inserting Data Into: silver.erp_cust_az12';
-    		INSERT INTO silver.erp_cust_az12 (
-    			cid,
-    			bdate,
-    			gen
-    		)
-    		SELECT
-    			CASE
-    				WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid)) -- Remove 'NAS' prefix if present
-    				ELSE cid
-    			END AS cid, 
-    			CASE
-    				WHEN bdate > GETDATE() THEN NULL
-    				ELSE bdate
-    			END AS bdate, -- Set future birthdates to NULL
-    			CASE
-    				WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
-    				WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
-    				ELSE 'n/a'
-    			END AS gen -- Normalize gender values and handle unknown cases
-    		FROM bronze.erp_cust_az12;
-    	    SET @end_time = GETDATE();
-            PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-            PRINT '>> -------------';
+		PRINT '>> Truncating Table: silver.erp_cust_az12';
+		TRUNCATE TABLE silver.erp_cust_az12;
+		PRINT '>> Inserting Data Into: silver.erp_cust_az12';
+		INSERT INTO silver.erp_cust_az12 (
+			cid,
+			bdate,
+			gen
+		)
+		SELECT
+			CASE
+				WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid)) -- Remove 'NAS' prefix if present
+				ELSE cid
+			END AS cid, 
+			CASE
+				WHEN bdate > GETDATE() THEN NULL
+				ELSE bdate
+			END AS bdate, -- Set future birthdates to NULL
+			CASE
+				WHEN UPPER(REPLACE(TRIM(gen), CHAR(13), '')) IN ('F', 'FEMALE') THEN 'Female'
+				WHEN UPPER(REPLACE(TRIM(gen), CHAR(13), '')) IN ('M', 'MALE') THEN 'Male'
+				ELSE 'n/a'
+			END AS gen -- Normalize gender values and handle unknown cases
+		FROM bronze.erp_cust_az12;
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
 
-    		PRINT '------------------------------------------------';
-    		PRINT '| Loading ERP Tables                            |';
-    		PRINT '------------------------------------------------';
-    
-            -- Loading erp_loc_a101
-            SET @start_time = GETDATE();
-    		PRINT '>> Truncating Table: silver.erp_loc_a101';
-    		TRUNCATE TABLE silver.erp_loc_a101;
-    		PRINT '>> Inserting Data Into: silver.erp_loc_a101';
-    		INSERT INTO silver.erp_loc_a101 (
-    			cid,
-    			cntry
-    		)
-    		SELECT
-    			REPLACE(cid, '-', '') AS cid, 
-    			CASE
-    				WHEN TRIM(cntry) = 'DE' THEN 'Germany'
-    				WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
-    				WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
-    				ELSE TRIM(cntry)
-    			END AS cntry -- Normalize and Handle missing or blank country codes
-    		FROM bronze.erp_loc_a101;
-    	    SET @end_time = GETDATE();
-            PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-            PRINT '>> -------------';
+		PRINT '------------------------------------------------';
+		PRINT '| Loading ERP Tables                            |';
+		PRINT '------------------------------------------------';
+
+		-- Loading erp_loc_a101
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.erp_loc_a101';
+		TRUNCATE TABLE silver.erp_loc_a101;
+		PRINT '>> Inserting Data Into: silver.erp_loc_a101';
+		INSERT INTO silver.erp_loc_a101 (
+			cid,
+			cntry
+		)
+		SELECT
+			REPLACE(cid, '-', '') AS cid, 
+			CASE
+				WHEN TRIM(REPLACE(cntry, CHAR(13), '')) = 'DE' THEN 'Germany'
+				WHEN TRIM(REPLACE(cntry, CHAR(13), '')) IN ('US', 'USA') THEN 'United States'
+				WHEN TRIM(REPLACE(cntry, CHAR(13), '')) = '' OR cntry IS NULL THEN 'n/a'
+				ELSE TRIM(REPLACE(cntry, CHAR(13), ''))
+			END AS cntry -- Normalize and Handle missing or blank country codes
+		FROM bronze.erp_loc_a101;
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
 		
-    		-- Loading erp_px_cat_g1v2
-    		SET @start_time = GETDATE();
-    		PRINT '>> Truncating Table: silver.erp_px_cat_g1v2';
-    		TRUNCATE TABLE silver.erp_px_cat_g1v2;
-    		PRINT '>> Inserting Data Into: silver.erp_px_cat_g1v2';
-    		INSERT INTO silver.erp_px_cat_g1v2 (
-    			id,
-    			cat,
-    			subcat,
-    			maintenance
-    		)
-    		SELECT
-    			id,
-    			cat,
-    			subcat,
-    			maintenance
-    		FROM bronze.erp_px_cat_g1v2;
-    		SET @end_time = GETDATE();
-    		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-            PRINT '>> -------------';
+		-- Loading erp_px_cat_g1v2
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.erp_px_cat_g1v2';
+		TRUNCATE TABLE silver.erp_px_cat_g1v2;
+		PRINT '>> Inserting Data Into: silver.erp_px_cat_g1v2';
+		INSERT INTO silver.erp_px_cat_g1v2 (
+			id,
+			cat,
+			subcat,
+			maintenance
+		)
+		SELECT
+			id,
+			cat,
+			subcat,
+			TRIM(REPLACE(maintenance, CHAR(13), '')) AS maintenance -- Remove carriage return characters from maintenance field
+		FROM bronze.erp_px_cat_g1v2;
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
     
-    		SET @batch_end_time = GETDATE();
-    		PRINT '=========================================='
-    		PRINT 'Loading Silver Layer is Completed';
-            PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
-    		PRINT '=========================================='
+		SET @batch_end_time = GETDATE();
+		PRINT '=========================================='
+		PRINT 'Loading Silver Layer is Completed';
+		PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '=========================================='
 		
-    	END TRY
-    	BEGIN CATCH
-    		PRINT '=========================================='
-    		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
-    		PRINT 'Error Message' + ERROR_MESSAGE();
-    		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
-    		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
-    		PRINT '=========================================='
-    	END CATCH
+	END TRY
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH
 END
