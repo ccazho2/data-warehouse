0a1136e80fce9aafee97702adda6ead71c288c01
ccazho2 <ccazho2@gmail.com>
2026-07-21 11:41:31 -0500

    metadata column to ddl_silver.sql tables
    addition of dwh_create_date DATETIME2 DEFAULT GETDATE() to each table

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -18,7 +18,8 @@ CREATE TABLE Silver.crm_cust_info (
     cst_lastname NVARCHAR(50),
     cst_marital_status NVARCHAR(20),
     cst_gndr NVARCHAR(10),
-    cst_create_date DATE
+    cst_create_date DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -32,7 +33,8 @@ CREATE TABLE Silver.crm_prd_info(
     prd_cost DECIMAL(10,2),
     prd_line NVARCHAR(50),
     prd_start_dt DATE,
-    prd_end_dt DATE
+    prd_end_dt DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -47,7 +49,8 @@ CREATE TABLE Silver.crm_sales_details (
     sls_due_dt DATE,
     sls_sales INT,
     sls_quantity INT,
-    sls_price INT
+    sls_price INT,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -56,7 +59,8 @@ IF OBJECT_ID('Silver.erp_cust_az12', 'U') IS NOT NULL
 CREATE TABLE Silver.erp_cust_az12 (
     CID NVARCHAR(50),
     BDATE DATE,
-    GEN NVARCHAR(50)
+    GEN NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -64,7 +68,8 @@ IF OBJECT_ID('Silver.erp_loc_a101', 'U') IS NOT NULL
     DROP TABLE Silver.erp_loc_a101;
 CREATE TABLE Silver.erp_loc_a101 (
     CID NVARCHAR(50),
-    CNTRY NVARCHAR(50)
+    CNTRY NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -74,5 +79,6 @@ CREATE TABLE Silver.erp_px_cat_g1v2 (
     ID NVARCHAR(50),
     CAT NVARCHAR(50),
     SUBCAT NVARCHAR(50),
-    MAINTENANCE NVARCHAR(50)
+    MAINTENANCE NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
